d85cf1fb1f81cd4a324b69630a7cac67c342c930
Louisa Evola <le9ds@virginia.edu>
2020-02-17 15:05:52 -0500

    login page styling

--- quickthooters/urls.py
+++ quickthooters/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('accounts/login/', TemplateView.as_view(template_name="login/index.html")),
+    path('login/', TemplateView.as_view(template_name="login/index.html")),
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
