b9b0e7ea6e55ca2535deae28ea8b767fbb420260
Naresha Namana <nnn4wu@virginia.edu>
2020-02-18 15:59:16 -0500

    hard-coded  payment w/ stripe API

--- quickthooters/urls.py
+++ quickthooters/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('maps', TemplateView.as_view(template_name="maps/map.html"), name = "maps")
+    path('maps', TemplateView.as_view(template_name="maps/map.html"), name = "maps"),
+    path('payments/', include('payments.urls')),
 ]
