fdff572ddde2c8ab11df0a5f7eed07ad03a3fed7
Louisa Evola <le9ds@virginia.edu>
2020-02-22 13:08:49 -0500

    creating and displaying user profile without styling

---
+++ login/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Profile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('user', 'major', 'year')
+
+    # def save(self, commit=True):
+    #     user = super().save(commit=False)
+    #     user.major = self.cleaned_data['major']
+    #     user.year = self.cleaned_data['year']
+    #
+    #     if commit:
+    #         user.save()
+    #     return user
+
+    def save(self, commit=True):
+        user = super(UserProfileForm, self).save(commit=False)
+        user.major = self.cleaned_data['major']
+        user.year = self.cleaned_data['year']
+        if commit:
+            user.save()
+        return user
