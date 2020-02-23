d6f822ffdce41e0307d913cd10d180196e355c14
asm3ft <59893493+asm3ft@users.noreply.github.com>
2020-02-23 11:17:25 -0500

    Update settings.py
    added heroku

--- quickthooters/settings.py
+++ quickthooters/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -173,3 +174,5 @@ SOCIALACCOUNT_STORE_TOKENS =True
 STRIPE_SECRET_KEY = 'sk_test_ALolSlccjW13vkO414UGD6e200xnJkVGhH'
 STRIPE_PUBLISHABLE_KEY = 'pk_test_qBsO9bCKqvUW9ZbY5bClNx1Z00a7RfZRz2'
 ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = False
+
+django_heroku.settings(locals())
