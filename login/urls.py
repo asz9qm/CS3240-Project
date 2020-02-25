29756e6889721385cffbd2cd123df558b0641994
Louisa Evola <le9ds@virginia.edu>
2020-02-24 20:05:02 -0500

    routed my profile and login based on user authentication status on navbar

--- login/urls.py
+++ login/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 app_name = 'login'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="login/index.html")),
+    path('', TemplateView.as_view(template_name="login/index.html"), name="display_login"),
     path('profile/', views.make_profile, name="make_profile"),
     path('info/', views.display_profile, name="display_profile"),
 ]
