620e4162be14fe5e427d3528b288444384190c39
ac5qm <ac5qm@virginia.edu>
2020-02-25 12:45:10 -0500

    Additional functionality to user profile

--- login/urls.py
+++ login/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
 from . import views
 from django.views.generic import TemplateView
+
 app_name = 'login'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="login/index.html"), name="display_login"),
     path('profile/', views.make_profile, name="make_profile"),
     path('info/', views.display_profile, name="display_profile"),
+    path('update/', views.update_profile, name="update_profile"),
 ]
