484a876352860f2bc319da57b4c5805befef52f6
asm3ft <59893493+asm3ft@users.noreply.github.com>
2020-03-13 19:15:20 -0400

    Update settings.py

--- quickthooters/settings.py
+++ quickthooters/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'TEST': {
             'NAME': 'test_db',
         },
-        'USER': 'name',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
