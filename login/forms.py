19dac697d72ab48371d142d183471dfe4383d3b0
ac5qm <ac5qm@virginia.edu>
2020-04-02 14:47:26 -0400

    Student-Requests Relation

--- login/forms.py
+++ login/forms.py
@@ -12,7 +12,6 @@ class UserProfileForm(forms.ModelForm):
         user = super(UserProfileForm, self).save(commit=False)
         user.major = self.cleaned_data['major']
         user.year = self.cleaned_data['year']
-        # if commit:
         user.save()
         return user
 
--- request/models.py
+++ request/models.py
@@ -1,9 +1,9 @@
-import datetime
 from django.db import models
-from django.utils import timezone
-from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django import forms
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
 
 # Create your models here.
 
@@ -11,9 +11,13 @@ from django import forms
 class Request(models.Model):
     subject = models.CharField(max_length=50)
     location = models.CharField(max_length=200)
-    specific = models.TextField()
+    specific = models.TextField(max_length=1000)
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    
     def __str__(self):
-        return self.subject
+        return self.author
+
     def get_absolute_url(self): 
         return reverse('main:maps')
 
