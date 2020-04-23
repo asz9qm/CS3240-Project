9408f4e34238e2efdd794c9d7c13c544ea783e71
Anna <asm3ft@virginia.edu>
2020-04-22 23:00:36 -0400

    testcases

--- ratings/tests.py
+++ ratings/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from .views import HomePageView
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# class LoginViewsTestCase(TestCase):
+#     def setUp(self):
+#         # Every test needs access to the request factory.
+#         self.factory = RequestFactory()
+#         self.user = User.objects.create_user(
+#             username='jacob', email='jacob@…', password='top_secret')
+
+#     def test_homepage_get(self):
+#         request = self.factory.get("")
+#         request.user = self.user
+#         response = HomePageView.as_view()(request)
+#         self.assertEquals(response.status_code, 200)
--- send_email/tests.py
+++ send_email/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from .views import mail
+from login.models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+# class EmailViewsTestCase(TestCase):
+#     def setUp(self):
+#         # Every test needs access to the request factory.
+#         self.factory = RequestFactory()
+#         self.user = User.objects.create_user(
+#             username='jacob', email='jacob@…', password='top_secret')
+
+#     def test_mail_display_get(self):
+#         request = self.factory.get('')
+#         request.user = self.user
+#         response = mail(request)
+#         self.assertEqual(response.status_code, 200)
+    
+#     def test_mail_display_post(self):
+#         request = self.factory.post('')
+#         request.user = self.user
+#         response = mail(request)
+#         self.assertEqual(response.status_code, 200)
+
